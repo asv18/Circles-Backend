1c490e68707a7f2b23d96804b4a91b8283f72056
Aarnav Srivastava <70161949+AarnavSrivastava@users.noreply.github.com>
2023-11-26 20:49:55 -0500

    Update database.ts

--- database.ts
+++ database.ts
@@ -4,16 +4,16 @@ import "https://deno.land/x/dotenv@v3.2.0/load.ts";
 import { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
 
 const database = new Client({
-    user: config().USER,
-    database: config().DATABASE,
-    hostname: config().HOST,
-    password: config().PASSWORD,
-    port: config().PORT
-    // user: Deno.env.get("USER"),
-    // database: Deno.env.get("DATABASE"),
-    // hostname: Deno.env.get("HOST"),
-    // password: Deno.env.get("PASSWORD"),
-    // port: Deno.env.get("PORT")
+    // user: config().USER,
+    // database: config().DATABASE,
+    // hostname: config().HOST,
+    // password: config().PASSWORD,
+    // port: config().PORT
+    user: Deno.env.get("USER"),
+    database: Deno.env.get("DATABASE"),
+    hostname: Deno.env.get("HOST"),
+    password: Deno.env.get("PASSWORD"),
+    port: Deno.env.get("PORT")
 });
 
 await database.connect();
